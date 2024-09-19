48657dbaf6a5617cb1a67e613a99d96e49429476
Guilherme <guibma@hotmail.com>
2024-09-19 14:19:48 -0300

    springboot

---
+++ ap1_bigdata_gerenciamento_cliente/src/main/java/br/edu/ap1bigdata/ap1_bigdata_gerenciamento_cliente/Ap1BigdataGerenciamentoClienteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ap1BigdataGerenciamentoClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ap1BigdataGerenciamentoClienteApplication.class, args);
+	}
+
+}
---
+++ ap1_bigdata_gerenciamento_cliente/src/test/java/br/edu/ap1bigdata/ap1_bigdata_gerenciamento_cliente/Ap1BigdataGerenciamentoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap1BigdataGerenciamentoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
