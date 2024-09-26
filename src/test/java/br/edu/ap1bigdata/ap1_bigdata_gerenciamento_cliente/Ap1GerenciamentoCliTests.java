68c9f97d7e541ce2a00d89d2621a46d5ef1ebf66
Guilherme <guibma@hotmail.com>
2024-09-26 15:12:49 -0300

    add testes endereco

--- src/test/java/br/edu/ap1bigdata/ap1_bigdata_gerenciamento_cliente/Ap1GerenciamentoCliTests.java
+++ src/test/java/br/edu/ap1bigdata/ap1_bigdata_gerenciamento_cliente/Ap1GerenciamentoCliTests.java
@@ -1,8 +1,9 @@
 package br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente;
-
 import br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente.Repositorio.ClienteRepositorio;
+import br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente.Repositorio.EnderecoRepositorio;
 import br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente.Servico.ClienteServico;
 import br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente.model.Cliente;
+import br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente.model.Endereco;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,10 +14,8 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-
 import java.time.LocalDate;
 import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -26,11 +25,28 @@ class Ap1GerenciamentoCliTests {
     @Mock
     private ClienteRepositorio clienteRepositorio;
 
+    @Mock
+    private EnderecoRepositorio enderecoRepositorio;
+
     @InjectMocks
     private ClienteServico clienteServico;
 
     private Validator validator;
 
+    public Cliente criarClienteValido() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("João Silva");
+        cliente.setEmail("joao.silva@email.com");
+        cliente.setCpf("123.456.789-10");
+        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));  // Cliente com mais de 18 anos
+        cliente.setTelefone("(21) 91234-5678");
+    
+        // Simula o salvamento do cliente no repositório
+        when(clienteRepositorio.save(cliente)).thenReturn(cliente);
+    
+        return cliente;
+    }
+    
     @BeforeEach
     public void setUp() {
     
@@ -39,6 +55,7 @@ class Ap1GerenciamentoCliTests {
         validator = factory.getValidator();
     }
 
+// Testes Cliente
     @Test
     public void deveFalharSeNomeForMenorQue3Caracteres() {
         Cliente cliente = new Cliente();
@@ -181,6 +198,170 @@ class Ap1GerenciamentoCliTests {
         assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("O telefone deve seguir o padrão (XX) XXXXX-XXXX")),
                 "Deve falhar pois o telefone não segue o formato (XX) XXXXX-XXXX");
         verify(clienteRepositorio, never()).save(any(Cliente.class));
+    }
+
+// Testes Endereco
+    @Test
+    public void deveFalharSeCepForInvalido() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua Principal");
+        endereco.setNumero("123");
+        endereco.setBairro("Centro");
+        endereco.setCep("1234567");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("CEP deve seguir o formato XXXXX-XXX")),
+                "Deve falhar pois o CEP não segue o formato XXXXX-XXX");
+
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeRuaEstiverEmBranco() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setRua("");
+        endereco.setNumero("123");
+        endereco.setBairro("Centro");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("Rua é obrigatória")),
+                "Deve falhar pois a rua está em branco");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeNumeroEstiverEmBranco() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua Principal");
+        endereco.setNumero("");
+        endereco.setBairro("Centro");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("Número é obrigatório")),
+                "Deve falhar pois o número está em branco");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeBairroEstiverEmBranco() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua Principal");
+        endereco.setNumero("123");
+        endereco.setBairro("");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("Bairro é obrigatório")),
+                "Deve falhar pois o bairro está em branco");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeEstadoForInvalido() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("XY");
+        endereco.setCidade("Rio de Janeiro");
+        endereco.setBairro("Centro");
+        endereco.setRua("Rua Principal");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("Estado inválido")),
+                "Deve falhar pois o estado é inválido");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeEstadoEstiverEmBranco() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("");
+        endereco.setCidade("Rio de Janeiro");
+        endereco.setBairro("Centro");
+        endereco.setRua("Rua Principal");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("Estado é obrigatório")),
+                "Deve falhar pois o estado está em branco");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeCidadeForMenorQue2Caracteres() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("RJ");
+        endereco.setCidade("A");
+        endereco.setBairro("Centro");
+        endereco.setRua("Rua Principal");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("A cidade deve ter entre 2 e 100 caracteres")),
+                "Deve falhar pois a cidade tem menos de 2 caracteres");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeCidadeEstiverEmBranco() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("RJ");
+        endereco.setCidade("");
+        endereco.setBairro("Centro");
+        endereco.setRua("Rua Principal");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("Cidade é obrigatória")),
+                "Deve falhar pois a cidade está em branco");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeBairroForMenorQue3Caracteres() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("RJ");
+        endereco.setCidade("Rio de Janeiro");
+        endereco.setBairro("AB");
+        endereco.setRua("Rua Principal");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("O bairro deve ter entre 3 e 100 caracteres")),
+                "Deve falhar pois o bairro tem menos de 3 caracteres");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeRuaForMenorQue3Caracteres() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("RJ");
+        endereco.setCidade("Rio de Janeiro");
+        endereco.setBairro("Centro");
+        endereco.setRua("AB");
+        endereco.setCep("12345-678");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("A rua deve ter entre 3 e 255 caracteres")),
+                "Deve falhar pois a rua tem menos de 3 caracteres");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
+    }
+    @Test
+    public void deveFalharSeCepEstiverEmBranco() {
+        Cliente cliente = criarClienteValido();
+        Endereco endereco = new Endereco();
+        endereco.setEstado("RJ");
+        endereco.setCidade("Rio de Janeiro");
+        endereco.setBairro("Centro");
+        endereco.setRua("Rua Principal");
+        endereco.setCep("");
+        endereco.setCliente(cliente);
+        Set<ConstraintViolation<Endereco>> violations = validator.validate(endereco);
+        assertTrue(violations.stream().anyMatch(v -> v.getMessage().equals("CEP é obrigatório")),
+                "Deve falhar pois o CEP está em branco");
+        verify(enderecoRepositorio, never()).save(any(Endereco.class));
     }
 
 }
--- src/test/java/br/edu/ap1bigdata/ap1_bigdata_gerenciamento_cliente/Controle/CliControleTeste.java
+++
@@ -1,6 +0,0 @@
-package br.edu.ap1bigdata.ap1_bigdata_gerenciamento_cliente.Controle;
-
-
-public class CliControleTeste {
-   
-}
